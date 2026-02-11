58eeb531be554d8162cbb26e1b56b91e9870c72a
DarkoZivadinovic <darko.zivadinovic98@gmail.com>
2026-02-11 23:01:17 +0100

    feat: add Book and Member models with encapsulation

---
+++ Models/Book.cs
@@ -0,0 +1,31 @@
+public class Book
+{
+    public int Id { get; private set; }
+    public string Title { get; private set; }
+    public string Author { get; private set; }
+    public bool IsBorrowed{ get ; private set;}
+    public Book(string title,string author)
+    {
+        if(string.IsNullOrWhiteSpace(title))
+            throw new ArgumentException("Title cannot be empty");
+
+        if(string.IsNullOrWhiteSpace(author))
+            throw new ArgumentException("Author cannot be empty");
+
+        Title = title;
+        Author = author;
+        IsBorrowed = false;
+    }
+    public void MarkAsBorrowed()
+    {
+        if (IsBorrowed)
+            throw new InvalidOperationException("Book is already borrowed");
+
+        IsBorrowed = true;
+    }
+
+    public void Return()
+    {
+        IsBorrowed = false;
+    }
+}
---
+++ Models/Member.cs
@@ -0,0 +1,20 @@
+public class Member
+{
+    private int Id {get; private set; }
+    
+    public string FirstName { get; private set; }
+    public string LastName { get; private set; }
+    public string FullName { get { return $"{FirstName} {LastName}";}}
+    public Member( string firstName, string lastName)
+    {
+        if (string.IsNullOrWhiteSpace(firstName))
+            throw new ArgumentException("FirstName cannot be empty");
+
+        if (string.IsNullOrWhiteSpace(lastName))
+            throw new ArgumentException("LastName cannot be empty");
+
+        FirstName = firstName;
+        LastName = lastName;
+    }
+  
+}
