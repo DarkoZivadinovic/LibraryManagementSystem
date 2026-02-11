a1760ef5bf4de6746ccd93d4ecef02418150b76f
DarkoZivadinovic <darko.zivadinovic98@gmail.com>
2026-02-11 23:11:06 +0100

    feat: add Book and Member models with encapsulation

---
+++ Models/Member.cs
@@ -0,0 +1,15 @@
+public class Member 
+{ 
+    private int Id { get; private set; }
+    public string FirstName { get; private set; }
+    public string LastName { get; private set; }
+    public string FullName { get { return $"{firstName} {lastName}";}} 
+    public Member( string firstName, string lastName) { 
+
+        if (string.IsNullOrWhiteSpace(firstName)) throw new ArgumentException("FirstName cannot be empty");
+        if (string.IsNullOrWhiteSpace(lastName)) throw new ArgumentException("LastName cannot be empty");
+        
+        FirstName = firstName;
+        LastName = lastName; 
+        } 
+    }
